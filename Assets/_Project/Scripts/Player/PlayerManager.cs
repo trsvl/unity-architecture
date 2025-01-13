fb8f98b17bec5616728f1995646af556b3306731
trsvl <vlokh.t4r4s@gmail.com>
2025-01-13 22:08:30 +0200

    add player movement logic

---
+++ Assets/_Project/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Player
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+
+        public void Move(Vector3 direction)
+        {
+            transform.position += direction * (Time.deltaTime * speed);
+        }
+    }
+}

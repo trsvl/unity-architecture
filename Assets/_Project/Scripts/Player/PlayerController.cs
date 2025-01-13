e1a898d5f81762c46229d748dbf51f6e7954a4c9
trsvl <vlokh.t4r4s@gmail.com>
2025-01-13 22:08:30 +0200

    add player movement logic

---
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using _Project.Scripts.GameSystemLogic.Interfaces;
+using UnityEngine;
+
+namespace _Project.Scripts.Player
+{
+    public class PlayerController : MonoBehaviour, IStartGame, IFinishGame
+    {
+        private PlayerInput _playerInput;
+        private PlayerManager _playerManager;
+
+
+        public void Construct(PlayerInput playerInput, PlayerManager playerManager)
+        {
+            _playerInput = playerInput;
+            _playerManager = playerManager;
+        }
+
+        public void StartGame()
+        {
+            _playerInput.OnMove += Move;
+        }
+
+        public void FinishGame()
+        {
+            _playerInput.OnMove -= Move;
+        }
+
+        private void Move(Vector3 direction)
+        {
+            _playerManager.Move(direction);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using _Project.Scripts.GameSystemLogic.Interfaces;
+using UnityEngine;
+
+namespace _Project.Scripts.Player
+{
+    public class PlayerInput : MonoBehaviour, IStartGame, IFinishGame
+    {
+        public event Action<Vector3> OnMove;
+
+        private bool isActive;
+
+        public void StartGame()
+        {
+            isActive = true;
+        }
+
+        public void FinishGame()
+        {
+            isActive = false;
+        }
+
+        private void Update()
+        {
+            if (isActive)
+            {
+                HandleInputs();
+            }
+        }
+
+        private void HandleInputs()
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Move(Vector3.up);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Move(Vector3.down);
+            }
+        }
+
+        private void Move(Vector3 position)
+        {
+            OnMove?.Invoke(position);
+        }
+    }
+}

756c6e0fc694af7fd1f2b88332b0d271747b4bce
trsvl <vlokh.t4r4s@gmail.com>
2025-01-13 22:09:29 +0200

    add game states, system logic

---
+++ Assets/_Project/Scripts/GameSystemLogic/GameLocator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Project.Scripts.GameSystemLogic
+{
+    public class GameLocator : MonoBehaviour
+    {
+        private readonly List<object> services = new();
+
+        public void AddService(object service)
+        {
+            services.Add(service);
+        }
+
+        public void RemoveService(object service)
+        {
+            services.Remove(service);
+        }
+
+        public T GetService<T>()
+        {
+            foreach (var service in services)
+            {
+                if (service is T result)
+                {
+                    return result;
+                }
+            }
+
+            throw new Exception($"Service of type {typeof(T)} is not found!");
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/GameSystemLogic/Interfaces/GameStates.cs
@@ -0,0 +1,30 @@
+﻿namespace _Project.Scripts.GameSystemLogic.Interfaces
+{
+    public enum GameState
+    {
+        OFF,
+        PLAY,
+        PAUSE,
+        FINISH
+    }
+
+    public interface IStartGame
+    {
+        public void StartGame();
+    }
+
+    public interface IPauseGame
+    {
+        public void PauseGame();
+    }
+
+    public interface IResumeGame
+    {
+        public void ResumeGame();
+    }
+
+    public interface IFinishGame
+    {
+        public void FinishGame();
+    }
+}

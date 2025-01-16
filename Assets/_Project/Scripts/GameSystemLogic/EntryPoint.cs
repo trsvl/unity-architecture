753a3640850f9779dfc865bead10fd1db3d0de0d
trsvl <vlokh.t4r4s@gmail.com>
2025-01-17 00:27:36 +0200

    add EntryPoint

---
+++ Assets/_Project/Scripts/GameSystemLogic/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.GameSystemLogic
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        [SerializeField] private MonoBehaviour[] _installers;
+
+        private void Awake()
+        {
+            foreach (var installer in _installers)
+            {
+                if (installer != null && installer is IInstaller installerComponent)
+                {
+                    installerComponent.Register().Wait();
+                }
+            }
+        }
+    }
+}

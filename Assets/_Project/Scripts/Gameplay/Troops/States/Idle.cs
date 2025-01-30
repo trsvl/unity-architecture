6552c51c1ec0b6b204b50cef51899e64896342d6
trsvl <vlokh.t4r4s@gmail.com>
2025-01-30 22:48:37 +0200

    add idle state for troops

---
+++ Assets/_Project/Scripts/Gameplay/Troops/States/Idle.cs
@@ -0,0 +1,10 @@
+namespace _Project.Scripts.Gameplay.Troops
+{
+    public class Idle : TroopStateNode
+    {
+        public Idle(IAnimationListener animationListener) : base(
+            animationListener)
+        {
+        }
+    }
+}

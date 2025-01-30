2030616de4e224c6261f5cf44d78bf29f9dbf246
trsvl <vlokh.t4r4s@gmail.com>
2025-01-30 18:13:11 +0200

    implement attack timer for troops

--- Assets/_Project/Scripts/Gameplay/Troops/AttackingTroop/AttackingTroopStateMachine.cs
+++ Assets/_Project/Scripts/Gameplay/Troops/AttackingTroop/AttackingTroopStateMachine.cs
@@ -1,4 +1,5 @@
 using _Project.Scripts.Gameplay.Troops;
+using _Project.Scripts.Utils;
 using UnityEngine;
 
 namespace _Project.Scripts.Gameplay._troops
@@ -7,12 +8,14 @@ namespace _Project.Scripts.Gameplay._troops
     {
         private readonly AttackingTroop _troop;
         private readonly AttackingTroopAnimationListener _animationListener;
+        private readonly StopWatchTimer _attackTimer;
 
 
-        public AttackingTroopStateMachine(AttackingTroop troop, Animator animator)
+        public AttackingTroopStateMachine(AttackingTroop troop, Animator animator, StopWatchTimer attackTimer)
         {
             _troop = troop;
             _animationListener = new AttackingTroopAnimationListener(animator);
+            _attackTimer = attackTimer;
         }
 
         public StateMachine GetStateMachine()
@@ -67,7 +70,7 @@ namespace _Project.Scripts.Gameplay._troops
 
         private StateNode Attack()
         {
-            var attack = new Attack(_animationListener, _troop);
+            var attack = new Attack(_animationListener, _troop, _attackTimer);
 
             StateNode stateNode = new StateNode(attack, Condition);
             return stateNode;
--- Assets/_Project/Scripts/Gameplay/Troops/States/Attack.cs
+++ Assets/_Project/Scripts/Gameplay/Troops/States/Attack.cs
@@ -1,30 +1,33 @@
+using _Project.Scripts.Utils;
+using UnityEngine;
+
 namespace _Project.Scripts.Gameplay.Troops
 {
     public class Attack : TroopStateNode
     {
         private readonly AttackingTroop _troop;
+        private readonly StopWatchTimer _attackTimer;
 
 
-        public Attack(IAnimationListener animationListener, AttackingTroop troop) : base(animationListener)
+        public Attack(IAnimationListener animationListener, AttackingTroop troop, StopWatchTimer attackTimer) : base(
+            animationListener)
         {
             _troop = troop;
-        }
-
-        public override void OnEnter()
-        {
-            base.OnEnter();
-            //attack first time
+            _attackTimer = attackTimer;
         }
 
         public override void Update()
         {
-            base.Update();
-            //timer
-            //Hit()
+            if (_attackTimer.IsReady)
+            {
+                Hit();
+            }
         }
 
         private void Hit()
         {
+            Debug.Log("Hit");
+            _attackTimer.Reset();
             _troop.ClosestDamageableTarget.TakeDamage(_troop.Config.AttackDamage);
         }
     }

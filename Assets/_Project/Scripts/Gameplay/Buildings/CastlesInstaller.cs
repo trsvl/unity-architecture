8d32c6d52f3543e7bc741b255bea473c63416c07
trsvl <vlokh.t4r4s@gmail.com>
2025-02-03 18:13:42 +0200

    refactor castle logic

--- Assets/_Project/Scripts/Gameplay/Buildings/CastlesInstaller.cs
+++ Assets/_Project/Scripts/Gameplay/Buildings/CastlesInstaller.cs
@@ -13,17 +13,20 @@ namespace _Project.Scripts.Gameplay.Buildings
 
         public void Register(Container container)
         {
-            var playerCastle = Instantiate(castlePrefab, leftCastleSpawnPoint);
-            var enemyCastle = Instantiate(castlePrefab, rightCastleSpawnPoint);
-
             var castleEvents = container.GetService<CastleEvents>();
             var gameplayStateObserver = container.GetService<GameplayStateObserver>();
-            
-            playerCastle.transform.position = leftCastleSpawnPoint.position;
-            enemyCastle.transform.position = rightCastleSpawnPoint.position;
 
-            playerCastle.Init(5f, Team.Player, castleEvents, gameplayStateObserver);
-            enemyCastle.Init(5f, Team.Enemy, castleEvents, gameplayStateObserver);
+            SpawnCastle(Team.Player, castleEvents, gameplayStateObserver, leftCastleSpawnPoint);
+            SpawnCastle(Team.Enemy, castleEvents, gameplayStateObserver, rightCastleSpawnPoint);
+        }
+
+        private void SpawnCastle(Team team, CastleEvents castleEvents, GameplayStateObserver gameplayStateObserver,
+            Transform castleSpawnPoint)
+        {
+            var castle = Instantiate(castlePrefab);
+            var objCollider = castle.GetComponent<Collider2D>();
+            castle.transform.position = castleSpawnPoint.position;
+            castle.Init(objCollider, 5f, team, castleEvents, gameplayStateObserver);
         }
     }
 }

ad980b732fc20662ec1651812d8851a81a9620b3
trsvl <vlokh.t4r4s@gmail.com>
2025-02-03 22:24:35 +0200

    add TroopCardsInstaller logic

--- Assets/_Project/Scripts/Gameplay/UI/Troop Card/CardButtonController.cs
+++ Assets/_Project/Scripts/Gameplay/UI/Troop Card/CardButtonController.cs
@@ -1,49 +1,49 @@
-using System;
-using System.Collections.Generic;
-using _Project.Scripts.Gameplay;
 using _Project.Scripts.Gameplay.Troops;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CardButtonController : MonoBehaviour
+namespace _Project.Scripts.Gameplay.UI.Troop_Card
 {
-    [SerializeField] private Image _cardCover;
-    [SerializeField] private Image _troopRadialFilled;
-    [SerializeField] private float _cooldown;
-    [SerializeField] private AttackingTroopConfig troop;
-    [SerializeField] private Team team;
-    
-
-    private Vector3 _originalScale;
-    private Button _cardButton;
-
-    private void Awake()
+    public class CardButtonController : MonoBehaviour
     {
-        _cardButton = GetComponent<Button>(); //!!!
-        _originalScale = transform.localScale;
-        _troopRadialFilled.color = new Color32(0, 0, 0, 208);
-        _cardButton.onClick.AddListener(OnPress);
-    }
+        [SerializeField] private Image _cardCover;
+        [SerializeField] private Image _troopIcon;
+        [SerializeField] private Image _troopIconRadialFilled;
 
-    public void Init(Button cardButton)
-    {
-        _cardButton = cardButton;
-    }
+        private TroopBaseConfig _troop;
+         private Team _team;
+        private Vector3 _originalScale;
+        private Button _cardButton;
 
-    private void OnPress()
-    {
-        Factory.Instance.Spawn(troop, team);//!!!
-        
-        _cardButton.interactable = false;
-        _cardCover.color = new Color32(161, 161, 161, 255);
-        _troopRadialFilled.fillAmount = 1;
-        _troopRadialFilled.DOFillAmount(0f, _cooldown).SetEase(Ease.Linear).OnComplete(() =>
+
+        public void Init(TroopBaseConfig troop, Team team, Button cardButton, Sprite troopIcon)
         {
-            _cardButton.interactable = true;
-            _cardCover.color = new Color32(255, 255, 255, 255);
-            transform.DOScale(_originalScale * 1.2f, 0.1f)
-                .OnComplete(() => { transform.DOScale(_originalScale, 0.1f); });
-        });
+            _troop = troop;
+            _team = team;
+            _cardButton = cardButton;
+            _troopIcon.sprite = troopIcon;
+
+            _originalScale = transform.localScale;
+            _troopIconRadialFilled.color = new Color32(0, 0, 0, 208);
+            _troopIconRadialFilled.sprite = troopIcon;
+            _cardButton.onClick.AddListener(OnPress);
+        }
+
+        private void OnPress()
+        {
+            Factory.Instance.Spawn(_troop, _team);
+
+            _cardButton.interactable = false;
+            _cardCover.color = new Color32(161, 161, 161, 255);
+            _troopIconRadialFilled.fillAmount = 1;
+            _troopIconRadialFilled.DOFillAmount(0f, _troop.SpawnCooldown).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                _cardButton.interactable = true;
+                _cardCover.color = new Color32(255, 255, 255, 255);
+                transform.DOScale(_originalScale * 1.2f, 0.1f)
+                    .OnComplete(() => { transform.DOScale(_originalScale, 0.1f); });
+            });
+        }
     }
 }
